c919ef95677dd7c555c2810bde77a6fa8d5a4158
Justinfan591 <87392740+Justinfan591@users.noreply.github.com>
2026-09-06 04:54:54 -0700

    Add: clone-graph - submission-5

---
+++ Data Structures & Algorithms/clone-graph/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        old_to_new = {}
+
+        def dfs(cur):
+            if cur in old_to_new:
+                return old_to_new[cur]
+
+            clone = Node(cur.val)
+            old_to_new[cur] = clone          # register BEFORE recursing
+
+            for nei in cur.neighbors:
+                clone.neighbors.append(dfs(nei))
+
+            return clone
+
+        return dfs(node)

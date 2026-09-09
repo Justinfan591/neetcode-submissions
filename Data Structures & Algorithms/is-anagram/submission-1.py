dd0e0176aad1ff2b5c20a42752c5159ef78c4fc1
Justinfan591 <87392740+Justinfan591@users.noreply.github.com>
2026-09-08 22:38:45 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dic = {}
+        dic2 = {}
+        for char in s: 
+            if char in dic: 
+                dic[char] += 1
+            else: dic[char] = 1
+        for char in t: 
+            if char in dic2: 
+                dic2[char] += 1
+            else: dic2[char] = 1
+        return dic==dic2

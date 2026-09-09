feb879e003d440bae1b9fbb66257e92c1fc8697d
Justinfan591 <87392740+Justinfan591@users.noreply.github.com>
2026-09-09 09:57:09 -0700

    Update: products-of-array-discluding-self - submission-9

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-9.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-9.py
@@ -1,26 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        total = 1
-        zeros = 0
-        location = 0
-        res = []
-        for i in range(len(nums)): 
-            if nums[i] == 0: 
-                zeros += 1
-                location = i
-            else: total = total * nums[i]
-        
-        #if more than 2 zeros, whole string would be 0
-        for i in range(len(nums)): 
-            if zeros >=2:
-                res.append(0)
-            elif zeros == 1: 
-                if i != location:
-                    res.append(0)
-                else: res.append(total)
-            elif zeros == 0:
-                res.append(total//nums[i])
-        
+        #iterate twice
+        n = len(nums)
+        res = [1] * n
+        prefix, posfix= 1,1
+        for i in range(n):
+            res[i] = res[i] * prefix
+            prefix = prefix * nums[i]
+        for i in range(n): 
+            res[n-i-1] = res[n-i-1] * posfix
+            posfix = posfix * nums[n-i-1]
         return res
+
                 
         

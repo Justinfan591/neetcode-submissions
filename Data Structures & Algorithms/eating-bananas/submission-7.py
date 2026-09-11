32abba49b1b723df735c57b9751c5a38ab749cf9
Justinfan591 <87392740+Justinfan591@users.noreply.github.com>
2026-09-11 15:29:09 -0700

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # testout which numbers fit the best
+        l, r = 1, max(piles)
+
+        def time(speed):
+            time = 0
+            for pile in piles: 
+                time += math.ceil(pile/speed)
+            return time
+
+        while l <= r: 
+            mid = (l + r) // 2
+            if time(mid) <= h: 
+                if mid == 1 or time(mid - 1) > h: #time used bigger than have
+                    return mid
+                r = mid - 1 # get a bigger time
+
+            else: l = mid + 1
+            
+        return l
